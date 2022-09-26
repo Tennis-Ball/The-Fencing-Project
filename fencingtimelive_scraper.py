dd7bbec734e0b08c4d546f3bf0760c638b2ff1f1
Mason Choi <choi.mason@gmail.com>
2022-09-25 22:25:15 -0400

    start notes and criteria

--- fencingtimelive_scraper.py
+++ fencingtimelive_scraper.py
@@ -1,3 +1,19 @@
+# Scrapes fencingtimelive for past tournament results data
+
+# date: location: tournamentname: event:
+# ["start:stop:size",[name by initial seed], [name by PRs], [name by FRs]]
+
+# date: location: tournamentname: event: fencername:
+
+
+
+
+# What would be interesting??
+# Distribution of fencing scores, also by age, region, rating, etc.
+# Percentage of timed out bouts based on age, region, rating, etc.
+# history of historically dominant/recessive fencers vs. each other
+# average length of event based on event and size
+# ^ all above by country/club
 import requests
 
 
